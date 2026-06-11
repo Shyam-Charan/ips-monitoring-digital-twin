2c0b8cb098a9b49e20e44c9f0e62d5b0f7b42b32
Shyam-Charan <147543976+Shyam-Charan@users.noreply.github.com>
2026-06-11 19:39:41 +0530

    Add firmware/main.c

---
+++ firmware/main.c
@@ -0,0 +1,12 @@
+#include "stm32f1xx.h"
+
+int main(void)
+{
+    // IPS Monitoring Digital Twin
+    // STM32F103 Blue Pill
+    while (1)
+    {
+        // Read fault signals and transmit
+    }
+    return 0;
+}
